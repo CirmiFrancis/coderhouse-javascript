a858a803597f805f6ccf4cc1d857c14d7f474bed
Francis Cirmi <cirmifrancis@gmail.com>
2023-12-22 20:19:31 -0300

    Pagina responsive. Librerias agregadas. Nuevos archivos. Y mas detalles...

--- assets/js/script.js
+++ assets/js/script.js
@@ -75,14 +75,117 @@ document.addEventListener("DOMContentLoaded", function () {
 // ==================================== SNOWFALL ====================================
 snowFall.snow(document.querySelector("body"));
 
-
-
-
 // ==================================== CLASE JUEGO ====================================
+
 class Juego {
     constructor(nombre, precio) {
-
+        this.nombre = nombre;
+        this.precio = precio;
     }
 }
 
-// ==================================== Buscador ====================================
+const aw2    = new Juego("Alan Wake 2",            34.39);
+const bg3    = new Juego("Baldur's Gate 3",        53.99);
+const ce     = new Juego("Celeste",                19.99);
+const cp2077 = new Juego("Cyberpunk 2077",         29.99);
+const er     = new Juego("EA Sports FC 24",        27.99);
+const ea24   = new Juego("Elden Ring",             35.99);
+
+const ff7r   = new Juego("Final Fantasy 7 Remake", 34.99);
+const fh5    = new Juego("Forza Horizon 5",        29.99);
+const gow    = new Juego("God of War",             24.99);
+const gta5   = new Juego("Grand Theft Auto V",     19.99);
+const hk     = new Juego("Hollow Knight",          7.49 );
+const mk1    = new Juego("Mortal Kombat 1",        34.99);
+
+const niera  = new Juego("Nier: Automata",         15.99);
+const phasmo = new Juego("Phasmophobia",           11.99);
+const rdr2   = new Juego("Red Dead Redemption 2",  19.79);
+const re4    = new Juego("Resident Evil 4",        29.99);
+const sek    = new Juego("Sekiro",                 29.99);
+const sf6    = new Juego("Street Fighter 6",       39.59);
+
+const spi    = new Juego("Spider-Man",             35.99);
+const terr   = new Juego("Terraria",               4.99 );
+
+let listaJuegos = [aw2,bg3,ce,cp2077,er,ea24,ff7r,fh5,gow,gta5,hk,mk1,niera,phasmo,rdr2,re4,sek,sf6,spi,terr]
+
+// ==================================== ELEMENTO JUEGO ====================================
+
+let divAW2 = document.getElementById('aw2');
+let divBG3 = document.getElementById('bg3');
+let divCE = document.getElementById('ce');
+let divCP2077 = document.getElementById('cp2077');
+let divEA24 = document.getElementById('ea24');
+let divER = document.getElementById('er');
+
+let divFF7R = document.getElementById('ff7r');
+let divFH5 = document.getElementById('fh5');
+let divGOW = document.getElementById('gow');
+let divGTA5 = document.getElementById('gta5');
+let divHK = document.getElementById('hk');
+let divMK1 = document.getElementById('mk1');
+
+let divNIERA = document.getElementById('niera');
+let divPHASMO = document.getElementById('phasmo');
+let divRDR2 = document.getElementById('rdr2');
+let divRE4 = document.getElementById('re4');
+let divSEK = document.getElementById('sek');
+let divSF6 = document.getElementById('sf6');
+
+let divSPI = document.getElementById('spi');
+let divTERR = document.getElementById('terr');
+
+
+
+// ==================================== BUSCADOR (Filtros) ====================================
+
+let buscarJuego = document.getElementById('buscarJuego');
+// let btnBuscar = document.getElementById('btnBuscar');
+
+// btnBuscar.addEventListener('click', function() {
+//     alert('Buscar juego.');
+// })
+
+buscarJuego.addEventListener('input', function() {
+    let cadenaBusqueda = buscarJuego.value.toLowerCase();
+    let juegosFiltrados = [...listaJuegos].filter(function (juego) {
+        return juego.nombre.toLowerCase().includes(cadenaBusqueda);
+    });
+
+    mostrarJuegosFiltrados(juegosFiltrados);
+})
+
+function mostrarJuegosFiltrados(juegos) {
+    resultado2.innerHTML = "";
+    if (juegos.length > 0) {
+        resultado2.innerHTML += "<ul>";
+        juegos.forEach(function (juego) {
+            resultado2.innerHTML += `<li>Tarea: ${juego.tarea}, Fecha: ${juego.fecha}</li>`;
+        });
+        resultado2.innerHTML += "</ul>";
+    } else {
+        resultado2.innerHTML = "No se encontraron tareas que coincidan con la búsqueda.";
+    }
+}
+
+
+
+
+// ==================================== CARRITO DE COMPRAS ====================================
+
+let listaCarritoCompras = []
+
+let btnAgregar = document.getElementById('agregarCarrito');
+
+btnAgregar.addEventListener('click', function() {
+    Swal.fire({
+        title: "JUEGO AGREGADO",
+        text: "Revisa el carrito de compras para ver la lista completa.",
+        icon: "success"
+      });
+})
+
+
+
+
